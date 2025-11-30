734ed9647a2a14a2cca3d419366b4ae0c8890281
Honsage <musatov.i.a@edu.mirea.ru>
2025-11-30 17:35:21 +0300

    feat: remove mock posts, create ORM models and migrate

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,1 +1,47 @@
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class PublishedModel(models.Model):
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
+
+class Category(PublishedModel):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
+
+class Location(PublishedModel):
+    name = models.CharField(max_length=256)
+
+
+class Post(PublishedModel):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    pub_date = models.DateTimeField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posts'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='posts'
+    )
