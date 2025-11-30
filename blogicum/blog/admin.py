26447c61fda9a2f7e359d6b0e2e2ba56f7ef5945
Honsage <musatov.i.a@edu.mirea.ru>
2025-11-30 17:58:34 +0300

    chore: configure localization for admin panel

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Location, Post
+
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Post)
--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -6,42 +6,73 @@ User = get_user_model()
 
 
 class PublishedModel(models.Model):
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField()
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(verbose_name='Добавлено')
 
     class Meta:
         abstract = True
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=256,
+                             verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text='Идентификатор страницы для URL; '
+        'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Location(PublishedModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256,
+                            verbose_name='Название места')
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
 
 class Post(PublishedModel):
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=256,
+                             verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем '
+        '— можно делать отложенные публикации.'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Категория'
     )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
