2b4f18a16fe7ba0e7a14d2b4d6eda94b29258b1c
Honsage <musatov.i.a@edu.mirea.ru>
2026-02-27 11:19:22 +0300

    feat: create app for user processing

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
